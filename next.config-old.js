6848eda8c807cbf37f36ebfd37a0d81227b927eb
PhilopateerITConsultancy <PhilopateerITConsultancy@gmail.com>
2025-05-22 00:43:03 +0300

    Add backup config file next.config-old.js

---
+++ next.config-old.js
@@ -0,0 +1,19 @@
+// C:\Users\ThinkPad x1 yoga\axa1-dapp\next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ This disables build failure on lint errors
+  },
+  // (include any existing config you have here)
+};
+
+module.exports = nextConfig;
+
+
+
+
+
+
+
+
+
