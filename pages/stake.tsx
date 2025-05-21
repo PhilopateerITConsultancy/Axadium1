b91d65531e542f75e98d32bde132e85ca677861b
PhilopateerITConsultancy <PhilopateerITConsultancy@gmail.com>
2025-05-22 00:52:37 +0300

    Fix missing pageName prop in Stake page

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -1,4 +1,19 @@
-import { Default } from 'components/layouts/Default';
+// C:\Users\ThinkPad x1 yoga\axa1-dapp\pages\stake.tsx
+
+import React from 'react';
+import Default from '@/layouts/Default';
+
+export default function StakePage() {
+  return (
+    <Default pageName="Stake">
+      <h1>Stake Page</h1>
+      <p>This is the staking interface.</p>
+    </Default>
+  );
+}
+
+
+/* import { Default } from 'components/layouts/Default';
 import { Home } from 'components/templates/home';
 import type { NextPage } from 'next';
 
@@ -10,4 +25,4 @@ export default function StakePage() {
       <p>This is the staking interface.</p>
     </Default>
   );
-}
+} */
